ed1352b06203f02d62193864e170729f8710f72f
Ismael <ext_ichumpitaz@delfingroupco.com.pe>
2024-05-27 12:08:37 -0500

    EMPEZANDO EL CARRUSEL

--- src/app/componentes/carrusel-component/carrusel-component.component.ts
+++ src/app/componentes/carrusel-component/carrusel-component.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrusel-component.component.css']
 })
 export class CarruselComponentComponent implements OnInit {
+  currentIndex = 0; 
+  carouselItems = [
+    { title: 'Item 1', imageUrl: '...' },
+    { title: 'Item 2', imageUrl: '...' },
+    { title: 'Item 2', imageUrl: '...' },
+    { title: 'Item 2', imageUrl: '...' },
+    { title: 'Item 2', imageUrl: '...' },
+    { title: 'Item 2', imageUrl: '...' },
+    // Agrega más elementos según sea necesario
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  navigate(direction: number) {
+    // Actualiza el índice actual según la dirección (izquierda o derecha)
+    this.currentIndex += direction;
+    // Asegúrate de que el índice no se salga de los límites del arreglo de elementos
+    const totalItems = this.carouselItems.length;
+    if (this.currentIndex < 0) {
+      this.currentIndex = totalItems - 1;
+    } else if (this.currentIndex >= totalItems) {
+      this.currentIndex = 0;
+    }
+  }
 }
